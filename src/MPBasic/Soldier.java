10b4a5761e1068da3ac2c1d6c267d50cf362be8f
David Lyons <dalyons@andrew.cmu.edu>
2022-01-03 23:13:46 -0500

    Soldiers report if they find an undiscovered Archon.

--- src/MPBasic/Soldier.java
+++ src/MPBasic/Soldier.java
@@ -15,6 +15,25 @@ public class Soldier extends Robot{
         Team opponent = rc.getTeam().opponent();
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
         if (enemies.length > 0) {
+            int theirArchons = Util.enemyArchonCount(rc.readSharedArray(0));
+            for (RobotInfo robot : enemies) {
+                MapLocation robotLoc = robot.getLocation();
+                if (theirArchons < rc.getArchonCount() && robot.getType() == RobotType.ARCHON) {
+                    boolean shouldInsert = true;
+                    for (int i = Util.firstEnemy; i < Util.firstEnemy + theirArchons; i++) {
+                        int testFlag = rc.readSharedArray(i);
+                        if (robotLoc.x == Util.xcoord(testFlag) && robotLoc.y == Util.ycoord(testFlag)) {
+                            shouldInsert = false;
+                            break;
+                        }
+                    }
+                    if (shouldInsert) {
+                        int locFlag = Util.storeMyLoc(robot);
+                        rc.writeSharedArray(Util.firstEnemy + theirArchons, locFlag);
+                        Util.incrementEnemy(rc);
+                    }
+                }
+            }
             MapLocation toAttack = enemies[0].location;
             if (rc.canAttack(toAttack)) {
                 rc.attack(toAttack);
