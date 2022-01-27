cdf4ea4b2adc82bf598e2b7d2915b6e4522bb582
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2022-01-27 17:40:33 -0500

    added comment for script

--- get_record.py
+++ get_record.py
@@ -1,3 +1,10 @@
+# paste the run from the queue tab into results.txt
+# start from the actual first match not the header "MPTempName vs Whoever"
+# run this script and look at results.csv
+# go into spreadsheet and click the cell you want to insert records at
+# go to file -> import -> upload -> results.csv -> replace data at current cell
+# bam the records are there
+
 import csv
 
 file = open('results.txt', 'r')
